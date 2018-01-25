b3e7ae91a84354e953e3a521389a7488e8d2b5af
Jérôme Guzzi <jerome@idsia.ch>
2018-01-25 10:25:15 +0100

    Linted and moved from wiringpi2 to wiringpi

--- ups/scripts/ups_node.py
+++ ups/scripts/ups_node.py
@@ -1,37 +1,35 @@
 #!/usr/bin/env python
 
-import wiringpi2
-import rospy
-from std_msgs.msg import Bool
 import subprocess
 
+import rospy
+import wiringpi
+from std_msgs.msg import Bool
 
-AC_OK=0
-BATT_OK=2
+AC_OK = 0
+BATT_OK = 2
 
-IN=0
-OUT=1
-
-
+IN = 0
+OUT = 1
 
 
 class ups_state_listener:
 
-    def read_pin(self,pin):
-        return int(subprocess.check_output(['/usr/local/bin/gpio','read',str(pin)]))        
+    def read_pin(self, pin):
+        return int(subprocess.check_output(['/usr/local/bin/gpio', 'read', str(pin)]))
 
-    def read_pin2(self,pin):
-        return wiringpi2.digitalRead()
+    def read_pin2(self, pin):
+        return wiringpi.digitalRead()
 
     def setup_gpio(self):
         # does not need root priviledge
         # the pin have to be already exported
         # for example with "gpio export <number> out"
-        wiringpi2.wiringPiSetupSys() 
+        wiringpi.wiringPiSetupSys()
         # does need root priviledge
-        #wiringpi2.wiringPiSetup() 
-        wiringpi2.pinMode(AC_OK,IN)
-        wiringpi2.pinMode(BATT_OK,IN)
+        # wiringpi.wiringPiSetup()
+        wiringpi.pinMode(AC_OK, IN)
+        wiringpi.pinMode(BATT_OK, IN)
 
     def __init__(self):
         rospy.init_node('ups', anonymous=True)
@@ -39,11 +37,12 @@ class ups_state_listener:
         batt_pub = rospy.Publisher('ups/battery', Bool, queue_size=1)
         rate = rospy.Rate(1)
         while not rospy.is_shutdown():
-            ac_ok=self.read_pin(AC_OK)
-            batt_ok=self.read_pin(BATT_OK)
+            ac_ok = self.read_pin(AC_OK)
+            batt_ok = self.read_pin(BATT_OK)
             ac_pub.publish(ac_ok)
             batt_pub.publish(batt_ok)
             rate.sleep()
+
 
 if __name__ == '__main__':
     try:
