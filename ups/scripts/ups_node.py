3cb90e2b720d1bba18ac6498ca55cdf1e600181c
AnonymSubmit <xy@z.com>
2018-03-06 13:38:45 +0100

    Moved to wiring pi 2

--- ups/scripts/ups_node.py
+++ ups/scripts/ups_node.py
@@ -1,51 +1,40 @@
 #!/usr/bin/env python
 
-import subprocess
-
 import rospy
-import wiringpi
+import wiringpi2 as wp
 from std_msgs.msg import Bool
 
 AC_OK = 0
 BATT_OK = 2
+POWER_LATCH = 3
 
 IN = 0
 OUT = 1
 
 
-class ups_state_listener:
+def setup_gpio(self):
+    wp.wiringPiSetup()
+    wp.pinMode(AC_OK, IN)
+    wp.pinMode(BATT_OK, IN)
+    wp.pinMode(POWER_LATCH, OUT)
+    wp.digitalWrite(POWER_LATCH, 1)
 
-    def read_pin(self, pin):
-        return int(subprocess.check_output(['/usr/local/bin/gpio', 'read', str(pin)]))
 
-    def read_pin2(self, pin):
-        return wiringpi.digitalRead()
-
-    def setup_gpio(self):
-        # does not need root priviledge
-        # the pin have to be already exported
-        # for example with "gpio export <number> out"
-        wiringpi.wiringPiSetupSys()
-        # does need root priviledge
-        # wiringpi.wiringPiSetup()
-        wiringpi.pinMode(AC_OK, IN)
-        wiringpi.pinMode(BATT_OK, IN)
+class ups_state_listener(object):
 
     def __init__(self):
         rospy.init_node('ups', anonymous=True)
-        ac_pub = rospy.Publisher('ups/ac', Bool, queue_size=1)
-        batt_pub = rospy.Publisher('ups/battery', Bool, queue_size=1)
-        rate = rospy.Rate(1)
-        while not rospy.is_shutdown():
-            ac_ok = self.read_pin(AC_OK)
-            batt_ok = self.read_pin(BATT_OK)
-            ac_pub.publish(ac_ok)
-            batt_pub.publish(batt_ok)
-            rate.sleep()
+        setup_gpio()
+        self.ac_pub = rospy.Publisher('ups/ac', Bool, queue_size=1)
+        self.batt_pub = rospy.Publisher('ups/battery', Bool, queue_size=1)
+        rospy.Timer(rospy.Duration(1), self.update)
+
+    def update(self, evt):
+        ac_ok = wp.digitalRead(AC_OK)
+        batt_ok = wp.digitalRead(BATT_OK)
+        self.ac_pub.publish(ac_ok)
+        self.batt_pub.publish(batt_ok)
 
 
 if __name__ == '__main__':
-    try:
-        ups_state_listener()
-    except rospy.ROSInterruptException:
-        pass
+    ups_state_listener()
