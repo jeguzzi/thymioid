aba9d9a4e038514f5c3e0f8420c0c05b823c0c58
Jerome Guzzi <jerome@idsia.ch>
2018-04-05 13:40:50 +0200

    Added rviz template

---
+++ thymioid/scripts/generate_rviz_config.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+
+def main():
+    path, name = sys.argv[1:3]
+    t_path = '{path}.{name}'.format(**locals())
+    # if os.path.exists(t_path):
+    #     return
+    with open(path) as f:
+        config = f.read()
+    print(config)
+    with open(t_path, 'w') as f:
+        f.write(config.format(**locals()))
+
+
+if __name__ == '__main__':
+    main()
