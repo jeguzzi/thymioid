2d36dcf8a791c7fcba13f78e61143747b82a2e69
Jerome Guzzi <jerome@idsia.ch>
2020-03-14 18:53:06 +0100

    Aded launch files

--- thymioid/setup.py
+++ thymioid/setup.py
@@ -11,6 +11,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/configuration', ['configuration/joy_config.yaml']),
         ('share/' + package_name + '/launch', glob('launch/*.launch'))
     ],
     install_requires=['setuptools'],
